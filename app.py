d948be14ce3d55ced1d71cfc384289c237c99b33
Rishabh Upadhayay <urishabh42@gmail.com>
2021-02-23 17:16:32 +0530

    login/signup

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from flask_cors import CORS, cross_origin
+import datetime
+import json
 
 #loading
 dataset = pd.read_csv('dataset.csv')
@@ -20,21 +22,74 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, rand
 knn_classifier = KNeighborsClassifier(n_neighbors = 8)
 knn_classifier.fit(X_train, y_train)
 
+#Global Dataset
+dataset = {}
+
+def readFromDatabase():
+    global dataset
+    with open('database.txt') as json_file:
+        dataset = json.load(json_file)
+
+def writeToDatabase():
+    global dataset
+    with open('database.txt', 'w') as outfile:
+        json.dump(dataset, outfile)
+    readFromDatabase()
+
+readFromDatabase()
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict/<string:username>', methods=['POST'])
 @cross_origin()
-def predict():
+def predict(username):
     data = request.json
     for key in data.keys():
         data[key] = [data[key]]
     df = pd.DataFrame.from_dict(data)
     ans = knn_classifier.predict(df)
+    data['result'] = int(ans[0])
+    data['dateTime'] = str(datetime.datetime.now())
+    if username not in dataset['history'].keys():
+        dataset['history'][username] = []
+    dataset['history'][username].append(data)
+    writeToDatabase()
     res = {'result': int(ans[0])}
     return jsonify(res)
+
+@app.route('/user/add', methods=['POST'])
+@cross_origin()
+def addUser():
+    data = request.json
+    userDict = {}
+    userDict['username'] = data['username']
+    userDict['password'] = data['password']
+    for i in dataset['users']:
+        if(i['username'] == userDict['username']):
+            return jsonify({'res': 'user exists'})
+    dataset['users'].append(userDict)
+    writeToDatabase()
+    res = {'res': 'ok'}
+    return jsonify(res)
+
+@app.route('/user/login/<string:username>/<string:password>', methods=['GET'])
+def login(username, password):
+    ok = False
+    for i in dataset['users']:
+        if(i['username'] == username and i['password'] == password):
+            ok = True
+
+    res = {'valid': ok}
+    return jsonify(res)
+
+@app.route('/history/<string:username>')
+def history(username):
+    if(username not in dataset['history'].keys()):
+        return jsonify({})
+    return jsonify(dataset['history'][username])
+
 
 
 app.run(threaded=True)
